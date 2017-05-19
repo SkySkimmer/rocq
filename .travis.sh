77ba313ba4023abba67141cb6f83bccf478b05d3
Gaetan Gilbert <gaetan.gilbert@ens-lyon.fr>
2017-05-19 19:45:08 +0200

    Move travis script to own file

---
+++ .travis.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+TEST_TARGET="$1"
+
+if [ "$TEST_TARGET" = validate ]
+then TW="travis_wait"
+fi
+
+set -ev
+
+echo 'Configuring Coq...' && echo -en 'travis_fold:start:coq.config\\r'
+./configure ${PREFIX} -usecamlp5 -native-compiler yes ${EXTRA_CONF}
+echo -en 'travis_fold:end:coq.config\\r'
+
+echo 'Building Coq...' && echo -en 'travis_fold:start:coq.build\\r'
+if [ "(" -d cache ")" -a "(" "$PREFIX" != "-local" ")" ]; then rm -rf cache; fi
+if [ ! "(" -d cache ")" ]; then make -j ${NJOBS}; else find cache; fi
+echo -en 'travis_fold:end:coq.build\\r'
+
+echo 'Running tests...' && echo -en 'travis_fold:start:coq.test\\r'
+${TW} make -j ${NJOBS} ${TEST_TARGET}
+echo -en 'travis_fold:end:coq.test\\r'
+test "$TEST_TARGET" != ci-geocoq
+
+set +ev
