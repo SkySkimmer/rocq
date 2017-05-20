ffd4db5c5d184ba712670b0ef984bbb9343d39ca
Gaetan Gilbert <gaetan.gilbert@ens-lyon.fr>
2017-05-20 23:18:53 +0200

    No forced failure on geocoq

--- .travis.sh
+++ .travis.sh
@@ -6,20 +6,25 @@ if [ "$TEST_TARGET" = install ]
 then PREFIX="-prefix cache"
 fi
 
-set -ev
+set -e
 
 echo 'Configuring Coq...' && echo -en 'travis_fold:start:coq.config\\r'
 ./configure ${PREFIX} -usecamlp5 -native-compiler yes ${EXTRA_CONF}
 echo -en 'travis_fold:end:coq.config\\r'
 
 echo 'Building Coq...' && echo -en 'travis_fold:start:coq.build\\r'
-if [ "(" -d cache ")" -a "(" "$PREFIX" != "-local" ")" ]; then rm -rf cache; fi
-if [ ! "(" -d cache ")" ]; then make -j ${NJOBS}; else find cache; fi
+if [ "(" -d cache ")" -a "(" "$PREFIX" != "-local" ")" ];
+then rm -rf cache;
+fi
+
+if [ ! "(" -d cache ")" ];
+then make -j ${NJOBS};
+else find cache;
+fi
 echo -en 'travis_fold:end:coq.build\\r'
 
 echo 'Running tests...' && echo -en 'travis_fold:start:coq.test\\r'
 make -j ${NJOBS} ${TEST_TARGET}
 echo -en 'travis_fold:end:coq.test\\r'
-test "$TEST_TARGET" != ci-geocoq
 
-set +ev
+set +e
